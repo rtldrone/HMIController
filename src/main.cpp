8573db40241cd50a7f49de4148bfff6b705efe8c
Cameron Earle <camearle20@me.com>
2019-11-12 23:36:40 -0500

    Added project files

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer webserver(80);
+AsyncWebSocket websocket("/ws");
+
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+
+}
+
+void setup() {
+    SPIFFS.begin();
+    webserver.serveStatic("/", SPIFFS, "/www/").setDefaultFile("index.html");
+    websocket.onEvent(onWsEvent);
+    webserver.addHandler(&websocket);
+    webserver.begin();
+}
+
+void loop() {
+
+}
